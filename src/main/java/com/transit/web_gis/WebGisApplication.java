fa0272e48a3b0ed88f8f954fcaa76ab19299571e
gwmoon <gwmoon@transitsolution.co.kr>
2025-01-06 21:55:48 +0900

    커뮤니티 버전 세팅

--- src/main/java/com/transit/web_gis/WebGisApplication.java
+++ src/main/java/com/transit/web_gis/WebGisApplication.java
@@ -3,12 +3,13 @@ package com.transit.web_gis;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @EnableAsync
 @SpringBootApplication
+@EnableWebMvc
 public class WebGisApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(WebGisApplication.class, args);
 	}
-
 }
